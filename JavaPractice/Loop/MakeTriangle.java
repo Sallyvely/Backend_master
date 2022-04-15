09694fc524525875728ed12d1c3db13dedfe988d
Sallyvely <tpdus4159@naver.com>
2022-04-15 22:03:03 +0900

    Update MakeTriangle.java

--- JavaPractice/Loop/MakeTriangle.java
+++ JavaPractice/Loop/MakeTriangle.java
@@ -5,7 +5,7 @@ import javax.security.auth.kerberos.KerberosCredMessage;
 public class MakeTriangle {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+
 		Scanner sc=new Scanner(System.in);
 		int n=sc.nextInt();
 
@@ -22,7 +22,11 @@ public class MakeTriangle {
 		 * for(int i=1; i<=n;i++) { for(int j=2;j<=i;j++) { System.out.print(" "); } for
 		 * (int k=n;k>=i;k--) { System.out.print("*"); } System.out.println(""); }
 		 */
-		
+		/* ´ëÄª »ï°¢Çü
+		 * for ( int i=1; i<=n;i++) { for(int j=1; j<=i; j++) { System.out.print("*"); }
+		 * System.out.println(""); } for (int i=1; i<=n; i++) { for(int j=n-1;j>=i;j--)
+		 * { System.out.print("*"); } System.out.println(""); }
+		 */
 		sc.close();
 	}
 
