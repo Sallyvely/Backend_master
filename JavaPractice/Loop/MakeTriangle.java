d8fa1c8c05f6057f3c9e4cd41e76faebb3d0546b
Sallyvely <tpdus4159@naver.com>
2022-04-15 21:59:20 +0900

    0415

--- JavaPractice/Loop/MakeTriangle.java
+++ JavaPractice/Loop/MakeTriangle.java
@@ -14,16 +14,14 @@ public class MakeTriangle {
 		 * System.out.println(""); }
 		 */
 		
-		for (int i=1; i<n+1; i++) {
-			for(int j=n; j>=i;j--) {
-				System.out.print("ㅁ");
-				for(int k=1;k<=i;k++) {
-				System.out.print("*");
-				}
-			
-			}
-			System.out.println("");
-		}
+		/* 역 삼각형
+		 * for (int i=1; i<n+1; i++) { for(int j=n; j>=i;j--) { System.out.print("ㅁ"); }
+		 * System.out.println(""); }
+		 */
+		/* 우측정렬 역 삼각형
+		 * for(int i=1; i<=n;i++) { for(int j=2;j<=i;j++) { System.out.print(" "); } for
+		 * (int k=n;k>=i;k--) { System.out.print("*"); } System.out.println(""); }
+		 */
 		
 		sc.close();
 	}
