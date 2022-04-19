f249e9d65d16ab991e942ce6f4d8e369216ec5b5
Sallyvely <tpdus4159@naver.com>
2022-04-19 22:04:44 +0900

    배열 left shift 연습

--- JavaPractice/Arr/Numberrotation.java
+++ JavaPractice/Arr/Numberrotation.java
@@ -1,24 +1,38 @@
-import java.awt.image.DataBufferInt;
-import java.util.Scanner;
 
+import java.util.Scanner;
 public class Numberrotation {
 
 	public static void main(String[] args) {
+		//n개의 숫자가 입력되면,
+
+		//n개의 숫자를 왼쪽으로 하나씩 돌려서 출력하시오.
 		Scanner sc = new Scanner(System.in);
 		int n=sc.nextInt();
 		int[] numarr=new int[n];
+		//배열 입력
 		for(int i=0; i<n; i++) {
 			numarr[i]=sc.nextInt();
 		}
-		int temp=numarr[0];
-		for(int i=n-1; i>=1;i--) {
-			numarr[i-1]=numarr[i];
-		}
-		numarr[n-1]=temp;
+		int k=1;
+		//입력된 배열 출력
 		for(int i=0; i<n; i++) {
 			System.out.print(numarr[i]+" ");
 		}
-		
+		System.out.println("");
+		//배열을 왼쪽으로 shift
+		while(k<n) {
+			
+			int temp=numarr[0]; //왼쪽으로 이동해야할 값을 temp에 저장
+			for(int i=0; i<n-1;i++) {
+				numarr[i]=numarr[i+1];
+			} // 왼쪽으로 이동
+			numarr[n-1]=temp; // 저장했던 값을 배열 맨 오른쪽에 지정
+			for(int i=0; i<n; i++) {
+				System.out.print(numarr[i]+" ");
+			}
+			System.out.println("");
+			k++;
+		}
 
 		sc.close();
 		
