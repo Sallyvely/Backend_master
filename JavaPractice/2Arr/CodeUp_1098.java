96a794fdeb01442acca16bd017853184c84254c6
Sallyvely <tpdus4159@naver.com>
2022-04-23 23:20:26 +0900

    Create CodeUp_1098.java

---
+++ JavaPractice/2Arr/CodeUp_1098.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class CodeUp_1098 {
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		String[][] arr=new String[];
+		
+
+	}
+
+}
