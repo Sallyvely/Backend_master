0900fd091cb3ef0fa07268af4df2737974888fc0
Sallyvely <tpdus4159@naver.com>
2022-05-04 16:11:55 +0900

    0504 구조체 연습문제

---
+++ JavaPractice/src/CodeUp_1805.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class CodeUp_1805 {
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		int[][] arr=new int[n][n];
+		for(int i=0; i<=n;i++) {
+			arr[0][i]
+			int a=sc.nextInt();
+			int b=sc.nextInt();
+			
+
+		
+		}
+		sc.close();
+
+	}
+
+}
