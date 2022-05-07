1ccc808d10a75abe11a4059ef595ac3081bc9a87
Sallyvely <tpdus4159@naver.com>
2022-05-07 15:06:14 +0900

    0507 재귀함수

--- JavaPractice/Practice/CodeUp_1905.java
+++ JavaPractice/Practice/CodeUp_1905.java
@@ -1,7 +1,23 @@
+import java.util.Scanner;
 public class CodeUp_1905 {
+	static int sum=0;
+	public static void Numberre(int n) {
 
-	public static void main(String[] args) {
 
+		if(n==0) {
+			System.out.println(sum);
+			return;
+		}
+		sum=sum+n;
+		Numberre(n-1);
+		
+	
 	}
 
+	public static void main(String[] args) {
+		Scanner scanner=new Scanner(System.in);
+		int n=scanner.nextInt();
+		Numberre(n);
+
+	}
 }
