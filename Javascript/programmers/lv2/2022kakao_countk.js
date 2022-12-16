9790b11d04b6842f4fb52ccf4f88311a132b1042
Sallyvely <tpdus4159@naver.com>
2022-12-16 21:51:00 +0900

    feat: 카카오 소수 갯수 구하기 문제 구현중

---
+++ Javascript/programmers/lv2/2022kakao_countk.js
@@ -0,0 +1,9 @@
+function solution(n, k) {
+    const answer = [];
+    let temp= n.toString(k);
+    let sosu = (n/2~~n)!=0;
+    
+    if(temp.charAt(i)/i+2!=0)
+        answer.push(temp.charAt(i));
+    return answer.length;
+}
