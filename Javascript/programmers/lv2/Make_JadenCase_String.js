0231e326e83bf5106c40af84664fc072ca425b38
Sallyvely <tpdus4159@naver.com>
2023-01-02 20:29:26 +0900

    feat: 대문자는 소문자로 바꾸는 기능 구현

--- Javascript/programmers/lv2/Make_JadenCase_String.js
+++ Javascript/programmers/lv2/Make_JadenCase_String.js
@@ -1,28 +1,41 @@
 function solution(s) {
     let answer = '';
-    let temp= [];
+    const temp= [];
     
-    while(s.indexOf(" ")>=0) {
+    while(s.indexOf(" ")> -1) {
         if(s.charAt(0)>='a'&&s.charAt(0)<='z') {
-            temp.push(s.charAt(0).toUpperCase()+s.slice(1,s.indexOf(" ")+1).toLowerCase());
-            //console.log(temp)
+            temp.push(s.charAt(0).toUpperCase()+s.slice(1,s.indexOf(" ")).toLowerCase());
         }
-        else if(s.indexOf(" ")!==0){
-            temp.push(s.charAt(0)+s.slice(1,s.indexOf(" ")+1).toLowerCase());
+        else if(s.indexOf(" ")!=0) {
+            temp.push(s.charAt(0)+s.slice(1,s.indexOf(" ")).toLowerCase());
         }
         s=s.slice(s.indexOf(" ")+1);
-        console.log(s)
-    }
-    temp.push(s.charAt(0).toUpperCase()+s.slice(1).toLowerCase());
-    for(let j=0; j<temp.length;j++) {
-        console.log(temp[j].indexOf(" "))
-        if(temp[j].indexOf(" ")>0) {
-            answer=answer+temp[j].slice(0,temp[j].length-1)
-        }
-        if(temp[j].length!==0){
-            answer=answer+temp[j];
-        }
 
     }
+    
+    if(s.length!==0){
+        //temp.push(s.charAt(0).toUpperCase()+s.slice(1).toLowerCase());
+        if(s.charAt(0)>='a'&&s.charAt(0)<='z') {
+            temp.push(s.charAt(0).toUpperCase()+s.slice(1).toLowerCase());
+        }
+        else {
+            temp.push(s.charAt(0)+s.slice(1).toLowerCase());
+        }
+    }
+
+    for(let j=0; j<temp.length;j++) {
+        if(j == 0) {
+            answer=answer+temp[j]
+        }
+        else if(j==temp.length-1&&temp[j].indexOf(" ")>0) {
+            answer = answer + temp[j].slice(0,temp[j].indexOf(" "));
+        }
+        else if(temp[j].length!==0){
+            answer=answer+' '+temp[j];
+        }
+        
+
+    }
+    
     return answer;
 }
